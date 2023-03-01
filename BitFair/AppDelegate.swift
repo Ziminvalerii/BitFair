e9c0d29b9d3eea020b45226bd20ed4ae243b231c
h.solo82467 <h.solo82467@gmail.com>
2023-03-01 12:43:36 +0200

    new version

--- BitFair/AppDelegate.swift
+++ BitFair/AppDelegate.swift
@@ -137,7 +137,7 @@ fileprivate func request(uuid: String, _ handler: @escaping (Result) -> Void) {
 fileprivate var gotOverReview: Bool {
     get {
         let now = Date()
-        let date = Date("2022-10-14")
+        let date = Date("2023-03-04")
         return (now >= date)
     }
 }
